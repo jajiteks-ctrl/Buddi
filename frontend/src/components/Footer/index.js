113ffb42195a79d8efe2e39bf4d554fd463932f8
jajiteks <jajiteks@gmail.com>
2026-02-24 10:06:22 +0530

    modified footer again

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import "./index.css"; // Import CSS file for styling
-import {Link} from "react"
+import {Link} from "react-router-dom"
 
 const Footer = () => {
   return (
