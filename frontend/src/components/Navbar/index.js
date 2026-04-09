3bc5b362d21d1ebee0b3b3208757d02acb71552e
jajiteks <jajiteks@gmail.com>
2026-04-09 13:06:55 +0530

    remove useeffect

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,6 +1,6 @@
 
 import { Link, useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState} from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 import "./index.css";
