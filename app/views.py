3542dd03e89718a76dbe9f92b5569f04157a5506
jajiteks-ctrl <jajiteks@gmail.com>
2026-04-28 22:49:45 +0530

    Update views.py update indentation

--- app/views.py
+++ app/views.py
@@ -401,11 +401,10 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
- class LogoutAPIView(CreateAPIView):
-     permission_classes = [IsAuthenticated]
-
-     def create(self, request, *args, **kwargs):
-         return Response({"detail": "Logged out successfully"})
+class LogoutAPIView(CreateAPIView):
+    permission_classes = [IsAuthenticated]
+    def create(self, request, *args, **kwargs):
+        return Response({"detail": "Logged out successfully"})
 
 # ---------------------------
 # Profile
