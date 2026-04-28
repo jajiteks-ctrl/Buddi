4483e7348b4921128af492d5556e7fbb751cdd53
jajiteks-ctrl <jajiteks@gmail.com>
2026-04-29 00:01:48 +0530

    Update settings.py set 0

--- backend/settings.py
+++ backend/settings.py
@@ -757,7 +757,7 @@ import dj_database_url
 DATABASES = {
     "default": dj_database_url.parse(
         os.environ.get("DATABASE_URL"),
-        conn_max_age=600
+        conn_max_age=0
     )
 }
 
